d75c3dce49d12af261459ca91fa17d8d1b8b4804
lihaitao <1775814590@qq.com>
2018-05-02 14:37:48 +0800

    add use config

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const Koa = require('koa')
 const app = new Koa()
 
+const config = require('config-lite')(__dirname)
+
 app.use(async (ctx,next)=>{
     ctx.response.type='text/html'
     ctx.response.body='<h1>hello koa!</h1>'
 })
 
-app.listen(3000,()=>{
+app.listen(config.port,()=>{
     console.log('server is running at http://localhost:3000')
 })
