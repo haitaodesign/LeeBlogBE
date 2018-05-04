5373924a887bbd1614ca0d76d9cd8bdc398370e1
lihaitao <15393243613@163.com>
2018-05-04 23:38:40 +0800

    delete require

--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,4 @@
 const router = require('koa-router')()
 const user = require('../controller/user')
-const {CustomError, HttpError} = require('../utils/customError')
 router.post('/user/add', user.add)
 module.exports = router
