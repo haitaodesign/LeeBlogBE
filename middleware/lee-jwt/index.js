feca35609c54401f0f5b4ff183ac793b8e750492
lihaitao <15393243613@163.com>
2018-09-20 23:02:57 +0800

    update 错误中间件修改

--- middleware/lee-jwt/index.js
+++ middleware/lee-jwt/index.js
@@ -10,7 +10,6 @@ export default () => {
         let payload
         try {
           payload = await verify(token, 'leehaitao') // 解密payload，获取用户名和ID
-          console.log(payload)
           ctx.state.user = {
             username: payload.username,
             _id: payload._id
@@ -19,19 +18,20 @@ export default () => {
           console.log('token verify fail: ', err)
         }
       }
-
       await next()
     } catch (err) {
-      console.log(err)
       if (err.status === 401) {
+        ctx.status = 401
         ctx.body = {
           code: -1,
-          message: '认证失败'
+          message: 'token认证失败'
         }
       } else {
         err.status = 404
-        ctx.body = '404'
-        console.log('不服就是怼：', err)
+        ctx.body = {
+          code: -1,
+          message: err
+        }
       }
     }
   }
