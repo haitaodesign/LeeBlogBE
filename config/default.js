1b1675527b6232f2c058d225fdee3f94c32b29c7
lihaitao <15393243613@163.com>
2018-11-10 23:08:57 +0800

    add 七牛配置文件

--- config/default.js
+++ config/default.js
@@ -4,5 +4,10 @@ module.exports = {
     host: 'localhost',
     port: '27017',
     name: 'blog'
+  },
+  qiniu: {
+    bucket: 'leeblogimg',
+    ak: '_EUicTCRIGMeEVRHCnU7FsvXNmy5TJTciIDk-JFl',
+    sk: 'UWWaFn4S5t3WfNwrOdqcA3YmWbRbCKgLDIiHkfYX'
   }
 }
