7ac2e05b06e320bbb06047a0b6e0408e04ab0fa4
lihaitao <15393243613@163.com>
2018-11-19 22:31:40 +0800

    add 上传文件同步到七牛云

--- controller/upload.js
+++ controller/upload.js
@@ -10,12 +10,14 @@ import {
   formData,
   middlewares
 } from 'koa-swagger-decorator'
+import UploadQiNiu from '../utils/upload.js'
+const cdnConfig = require('../config/default.js')
+const { imgDomain } = cdnConfig.qiniu
 const {
   CustomError
 } = require('../utils/customError')
 const constants = require('../utils/constants')
 const response = require('../utils/response')
-const { getToken } = require('../utils/qiniu.js')
 const QiNiuTag = tags(['七牛云'])
 export default class Upload {
   @request('post', '/upload')
@@ -31,7 +33,10 @@ export default class Upload {
   static async upload (ctx, next) {
     try {
       // 同步图片资源到七牛云
-      ctx.body = response(constants.CUSTOM_CODE.SUCCESS, {path: ctx.uploadpath}, '获取七牛云上传token成功！')
+      const path = ctx.request.body.files.file.path
+      const res = await UploadQiNiu.uploadQiNiu('public/upload/' + ctx.uploadpath.file, path)
+      const url = imgDomain + res.key
+      ctx.body = response(constants.CUSTOM_CODE.SUCCESS, {url}, '获取七牛云上传token成功！')
     } catch (error) {
       throw new CustomError(constants.HTTP_CODE.BAD_REQUEST, error.message)
     }
